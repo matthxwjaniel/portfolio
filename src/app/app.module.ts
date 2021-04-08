b8299b5460db0286de58855beba6cb3253dd510d
Matthew Feliciano <mathyoujaniel@gmail.com>
2021-04-09 04:27:40 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PortfolioComponent,
+    BlogComponent,
+    ContactComponent,
+    FooterComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
